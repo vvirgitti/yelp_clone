8dc40f6fcd63767527be2110b925fb6d37c94da3
Vanessa <virgitti@gmail.com>
2015-03-30 18:20:26 +0100

    created a new restaurant functionality

--- spec/features/restaurants_features_spec.rb
+++ spec/features/restaurants_features_spec.rb
@@ -25,6 +25,7 @@ feature 'restaurants' do
       visit '/restaurants'
       click_link 'Add a restaurant'
       fill_in 'Name', with: 'KFC'
+      click_button 'Create Restaurant'
       expect(page).to have_content 'KFC'
       expect(current_path).to eq '/restaurants'
     end
