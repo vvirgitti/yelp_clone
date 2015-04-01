f4f201b40ec8dd5be55df128639f6a2bf184a154
Vanessa <virgitti@gmail.com>
2015-04-01 16:13:58 +0100

    passing tests for adding average review

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,7 +5,7 @@ class Restaurant < ActiveRecord::Base
 
   def average_rating
     return 'N/A' if reviews.none?
-    4
+    reviews.inject(0) {|memo, review| memo + review.rating} / reviews.count 
   end
 
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -22,4 +22,13 @@ describe Restaurant, :type => :model do
     end
   end
 
+  context "multiple reviews" do
+    it 'returns the average' do
+      restaurant = Restaurant.create(name: "The Ivy")
+      restaurant.reviews.create(rating: 1)
+      restaurant.reviews.create(rating: 5)
+      expect(restaurant.average_rating).to eq 3
+    end
+  end
+
 end
