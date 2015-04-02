e6693e0b95b6f06cab2e7e3edc7ef687ef901a57
Vanessa <virgitti@gmail.com>
2015-04-02 14:26:17 +0100

    added ajax functionality

--- app/assets/javascripts/endorsements.js
+++ app/assets/javascripts/endorsements.js
@@ -1,15 +1,12 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).ready(function() {
 
-$(document).ready(function)() {
   $('.endorsements-link').on('click', function(event){
-    event.preventDefault();
+      event.preventDefault();
 
-    var endorsementCount = $(this).siblings('.endorsements_count');
+      var endorsementCount = $(this).siblings('.endorsements_count');
 
-    $.post(this.href, function(response){
-      endorsementCount.text(response.new_endosement_count);
-    });
-  });
-};
+      $.post(this.href, function(response){
+        endorsementCount.text(response.new_endorsement_count);
+    })
+  })
+})
